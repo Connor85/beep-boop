d1ea13a45252f0d0611a1d05137e1292981c7a75
cm <connormccarthy8@gmail.com>
2018-08-17 16:26:36 -0700

    returns beep/boop, but not rest of string

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,16 @@ var bbNumber = function (inputNumber){
   var rangeArr = Array.from({length: inputNumber}, (x,i) => i);
   rangeArr.push(inputNumber);
   rangeArr.shift();
+  var regexBeep = /0/gi;
+  var regexBoop = /1/gi;
   var numbers = rangeArr.toString();
-  alert(numbers);
     for (i = 0; i <= numbers.length; i++) {
       if (numbers % 3 === 0) {
-        alert(working);
         return ("I'm sorry, Dave. I'm afraid I can't do that.");
       } else if (numbers.includes("0")) {
-        return("Boop!");
+        return (numbers.replace(regexBeep, 'Beep!'));
       } else if (numbers.includes("1")) {
-        return("Beep!");
+        return (numbers.replace(regexBoop,'Boop!'));
       } else
       return (numbers);
 
@@ -28,3 +28,9 @@ $(document).ready(function() {
     $("#result").text(output);
   });
 });
+
+// var p = 'The quick brown fox jumped over the lazy dog. If the dog reacted, was it really lazy?';
+//
+// var regex = /dog/gi;
+//
+// console.log(p.replace(regex, 'ferret'));
