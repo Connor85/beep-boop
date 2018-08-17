36421eec3216a5d53f872312e7e2faea51bbc7a5
cm <connormccarthy8@gmail.com>
2018-08-17 16:10:22 -0700

    returns beep/boop, but not rest of string

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,15 @@ var bbNumber = function (inputNumber){
   var rangeArr = Array.from({length: inputNumber}, (x,i) => i);
   rangeArr.push(inputNumber);
   rangeArr.shift();
-  var numbers = rangeArr;
+  var numbers = rangeArr.toString();
+  alert(numbers);
     for (i = 0; i <= numbers.length; i++) {
       if (numbers % 3 === 0) {
+        alert(working);
         return ("I'm sorry, Dave. I'm afraid I can't do that.");
-      } else if (numbers === 1) {
+      } else if (numbers.includes("0")) {
         return("Boop!");
-      } else if (numbers === 0) {
+      } else if (numbers.includes("1")) {
         return("Beep!");
       } else
       return (numbers);
