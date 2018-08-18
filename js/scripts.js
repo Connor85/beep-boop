fc3dea9a7d43733e377ac785d6b389cf35c83782
cm <connormccarthy8@gmail.com>
2018-08-17 22:13:38 -0700

    add loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,28 @@
 // business logic
 var bbNumber = function (inputNumber){
-  if (inputNumber.includes("0")) {
-    return "Beep!";
-  } else if (inputNumber.includes("1")){
-    return "Boop!";
+  var outut = [];
+  if (inputNumber.match(/[a-z]/i)){
+    return ("System Overload!!!");
+  } else {
+    for (i = 0; i <= inputNumber; i++)
+    if (inputNumber.includes("0")) {
+      return "Beep!";
+    } else if (inputNumber.includes("1")){
+      return "Boop!";
+    } else if (inputNumber % 3 === 0){
+      return "I'm sorry, Dave. I'm afraid I can't do that."
+    }
   }
-    // else if (inputNumber.match(/[a-z]/i)){
-    //       return (false);
 
-    return (inputNumber);
-  };
+  return (inputNumber);
+};
 
-  // user logic
-  $(document).ready(function() {
-    $("form#beep-boop").submit(function(event) {
-      event.preventDefault();
-      var userInput = $("#inputtxt").val();
-      output = bbNumber(userInput);
-      $("#result").text(output);
-    });
+// user logic
+$(document).ready(function() {
+  $("form#beep-boop").submit(function(event) {
+    event.preventDefault();
+    var userInput = $("#inputtxt").val();
+    output = bbNumber(userInput);
+    $("#result").text(output);
   });
+});
