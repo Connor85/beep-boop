ab5b95a3607226e9624a80699d9bfd00212636fd
cm <connormccarthy8@gmail.com>
2018-08-17 19:24:37 -0700

    program returns Boop if 1 is input and beep if 0 is input

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,22 @@
 // business logic
 var bbNumber = function (inputNumber){
-    var numbers = []
-    if (inputNumber.match(/[a-z]/i)){
-      return (false);
-    }
-  return (inputNumber);
+  if (inputNumber.includes("0")) {
+    return "Beep!";
+  } else if (inputNumber.includes("1")){
+    return "Boop!";
+  }
+    // else if (inputNumber.match(/[a-z]/i)){
+    //       return (false);
+
+    return (inputNumber);
   };
 
-// user logic
-$(document).ready(function() {
-  $("form#beep-boop").submit(function(event) {
-    event.preventDefault();
-    var userInput = $("#inputtxt").val();
-    output = bbNumber(userInput);
-    $("#result").text(output);
+  // user logic
+  $(document).ready(function() {
+    $("form#beep-boop").submit(function(event) {
+      event.preventDefault();
+      var userInput = $("#inputtxt").val();
+      output = bbNumber(userInput);
+      $("#result").text(output);
+    });
   });
-});
