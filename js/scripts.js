769c343691a59f80059149f607a0acc825feb850
cm <connormccarthy8@gmail.com>
2018-08-17 23:51:10 -0700

    pass all specs. Fix output to display numbers in array, replace 1,2 and numbers divisible by three

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,23 @@
 // business logic
 var bbNumber = function (inputNumber){
-  var outut = [];
+  var output = [];
   if (inputNumber.match(/[a-z]/i)){
     return ("System Overload!!!");
   } else {
     for (i = 0; i <= inputNumber; i++){
-   if (inputNumber % 3 === 0){
-      return "I'm sorry, Dave. I'm afraid I can't do that."
-    } else if (inputNumber.includes("0")) {
-      return "Beep!";
-    } else if (inputNumber.includes("1")){
-      return "Boop!";
-     (inputNumber.includes("0"))
-      return "Beep!";
-    }
-}
-    return (inputNumber);
-  };
-};
+      if (i % 3 === 0 && i != 0) {
+            output.push("I'm sorry, Dave. I'm afraid I can't do that.");
+          } else if (i.toString().includes("1")) {
+            output.push("Boop!");
+          } else if (i.toString().includes("0")) {
+            output.push("Beep!");
+          } else {
+            output.push(i);
+          }
+        }
+        return output.join(", ");
+      };
+    };
   // user logic
   $(document).ready(function() {
     $("form#beep-boop").submit(function(event) {
