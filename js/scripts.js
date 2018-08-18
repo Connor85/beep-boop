8f66ded9a2962ccdc131dce8a190136ca5620ecc
cm <connormccarthy8@gmail.com>
2018-08-17 18:34:02 -0700

    pass 1st and 2nd spec. returns only numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,12 @@
 // business logic
 var bbNumber = function (inputNumber){
-  var rangeArr = Array.from({length: inputNumber}, (x,i) => i);
-  rangeArr.push(inputNumber);
-  rangeArr.shift();
-  var regexBeep = /0/gi;
-  var regexBoop = /1/gi;
-  var numbers = rangeArr.toString();
-    for (i = 0; i <= numbers.length; i++) {
-      if (numbers % 3 === 0) {
-        return ("I'm sorry, Dave. I'm afraid I can't do that.");
-      } else if (numbers.includes("0")) {
-        return (numbers.replace(regexBeep, 'Beep!'));
-      } else if (numbers.includes("1")) {
-        return (numbers.replace(regexBoop,'Boop!'));
-      } else
-      return (numbers);
-
+    var numbers = []
+    if (inputNumber.match(/[a-z]/i)){
+      return (false);
+    }
+  return (inputNumber);
   };
-  return (numbers);
-}
+
 // user logic
 $(document).ready(function() {
   $("form#beep-boop").submit(function(event) {
@@ -28,9 +16,3 @@ $(document).ready(function() {
     $("#result").text(output);
   });
 });
-
-// var p = 'The quick brown fox jumped over the lazy dog. If the dog reacted, was it really lazy?';
-//
-// var regex = /dog/gi;
-//
-// console.log(p.replace(regex, 'ferret'));
